4d595a8079b90a948d21e670f09dbdc819cba0e7
pranavp1616 <pranavp1616@gmail.com>
2020-08-28 01:16:48 +0530

    sqllit db for testing

--- mainProject/settings.py
+++ mainProject/settings.py
@@ -100,11 +100,13 @@ DATABASES = {
     }
 }
 
+'''
 # config for HEROKU - postgresql
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
- 
+'''
+
 '''
 #   AWS RDS (postgres) CONFIG
 DATABASES = {
